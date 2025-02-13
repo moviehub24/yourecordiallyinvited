b57d2bc9f85630f93547d645b2a13abc72fd665f
hub <harshadwasnik95298@gmail.com>
2025-02-13 20:23:11 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,198 @@
+bot.onText('bagheera', async (msg) => {
+  await handleMovieRequest(msg, 'bagheera', [55, 56, 57]);
+});
+
+bot.onText('spellbound', async (msg) => {
+  await handleMovieRequest(msg, 'spellbound', [2, 3, 4]);
+});
+
+bot.onText('redone', async (msg) => {
+  await handleMovieRequest(msg, 'redone', [49, 50, 51]);
+});
+bot.onText('shadowland', async (msg) => {
+  await handleMovieRequest(msg, `${msg.text}`, [95,91,94]);
+});
+
+bot.onText('chillichicken', async (msg) => {
+  await handleMovieRequest(msg, `${msg.text}`, [84,85,86]);
+});
+
+bot.onText('calendarkiller', async (msg) => {
+  await handleMovieRequest(msg, `${msg.text}`, [87,88]);
+});
+
+bot.onText('populartheory', async (msg) => {
+  await handleMovieRequest(msg, `${msg.text}`, [18,19,20]);
+});
+
+bot.onText('moana2', async (msg) => {
+  await handleMovieRequest(msg, `${msg.text}`, [21,22,23]);
+});
+
+bot.onText('mufasathelionking', async (msg) => {
+  await handleMovieRequest(msg, `${msg.text}`, [25,26,27]);
+});
+
+bot.onText('kraventhehunter', async (msg) => {
+  await handleMovieRequest(msg, `${msg.text}`, [24,31,32]);
+});
+
+bot.onText('sonicthehedgehog3', async (msg) => {
+  await handleMovieRequest(msg, `${msg.text}`, [33,34,35]);
+});
+
+bot.onText('gladiator2', async (msg) => {
+  await handleMovieRequest(msg, `${msg.text}`, [36,37,38]);
+});
+
+bot.onText('amaran', async (msg) => {
+  await handleMovieRequest(msg, `${msg.text}`, [40,41,42]);
+});
+
+bot.onText('operationbloodhunt', async (msg) => {
+  await handleMovieRequest(msg, `${msg.text}`, [43,44,45]);
+});
+
+bot.onText('gaami', async (msg) => {
+  await handleMovieRequest(msg, `${msg.text}`, [46,47,48]);
+});
+
+bot.onText('wicked', async (msg) => {
+  await handleMovieRequest(msg, `${msg.text}`, [52,53,54]);
+});
+
+bot.onText('wallaceandgromitvengeancemostfowl', async (msg) => {
+  await handleMovieRequest(msg, `${msg.text}`, [58,59,60]);
+});
+
+bot.onText('gamechanger', async (msg) => {
+  await handleMovieRequest(msg, `${msg.text}`, [63,66,67]);
+});
+
+bot.onText('shukrana', async (msg) => {
+  await handleMovieRequest(msg, `${msg.text}`, [64,68,69]);
+});
+
+bot.onText('sorgavaasal', async (msg) => {
+  await handleMovieRequest(msg, `${msg.text}`, [70,65,71]);
+});
+
+bot.onText('backinaction', async (msg) => {
+  await handleMovieRequest(msg, `${msg.text}`, [72,73,74]);
+});
+
+bot.onText('unstoppable', async (msg) => {
+  await handleMovieRequest(msg, `${msg.text}`, [75,76,77]);
+});
+
+bot.onText('thelovescam', async (msg) => {
+  await handleMovieRequest(msg, `${msg.text}`, [78,79,80]);
+});
+
+bot.onText('hellboythecrookedman', async (msg) => {
+  await handleMovieRequest(msg, `${msg.text}`, [81,82,83]);
+});
+
+bot.onText('advitam', async (msg) => {
+  await handleMovieRequest(msg, `${msg.text}`, [89,90,92]);
+});
+
+bot.onText('nighthunter', async (msg) => {
+  await handleMovieRequest(msg, `${msg.text}`, [95,96,97]);
+});
+
+bot.onText('',async(msg)=>{
+  await handleMovieRequest(msg,`${msg.text}`, [])
+});
+
+bot.onText('',async(msg)=>{
+  await handleMovieRequest(msg,`${msg.text}`, [])
+});
+
+bot.onText('',async(msg)=>{
+  await handleMovieRequest(msg,`${msg.text}`, [])
+});
+
+bot.onText('',async(msg)=>{
+  await handleMovieRequest(msg,`${msg.text}`, [])
+});
+
+bot.onText('',async(msg)=>{
+  await handleMovieRequest(msg,`${msg.text}`, [])
+});
+
+bot.onText('',async(msg)=>{
+  await handleMovieRequest(msg,`${msg.text}`, [])
+});
+
+bot.onText('',async(msg)=>{
+  await handleMovieRequest(msg,`${msg.text}`, [])
+});
+
+bot.onText('',async(msg)=>{
+  await handleMovieRequest(msg,`${msg.text}`, [])
+});
+
+bot.onText('',async(msg)=>{
+  await handleMovieRequest(msg,`${msg.text}`, [])
+});
+
+bot.onText('',async(msg)=>{
+  await handleMovieRequest(msg,`${msg.text}`, [])
+});
+
+bot.onText('',async(msg)=>{
+  await handleMovieRequest(msg,`${msg.text}`, [])
+});
+
+bot.onText('',async(msg)=>{
+  await handleMovieRequest(msg,`${msg.text}`, [])
+});
+
+bot.onText('',async(msg)=>{
+  await handleMovieRequest(msg,`${msg.text}`, [])
+});
+
+bot.onText('',async(msg)=>{
+  await handleMovieRequest(msg,`${msg.text}`, [])
+});
+
+bot.onText('',async(msg)=>{
+  await handleMovieRequest(msg,`${msg.text}`, [])
+});
+
+bot.onText('',async(msg)=>{
+  await handleMovieRequest(msg,`${msg.text}`, [])
+});
+
+bot.onText('',async(msg)=>{
+  await handleMovieRequest(msg,`${msg.text}`, [])
+});
+
+bot.onText('',async(msg)=>{
+  await handleMovieRequest(msg,`${msg.text}`, [])
+});
+
+bot.onText('',async(msg)=>{
+  await handleMovieRequest(msg,`${msg.text}`, [])
+});
+
+bot.onText('',async(msg)=>{
+  await handleMovieRequest(msg,`${msg.text}`, [])
+});
+
+bot.onText('',async(msg)=>{
+  await handleMovieRequest(msg,`${msg.text}`, [])
+});
+
+bot.onText('',async(msg)=>{
+  await handleMovieRequest(msg,`${msg.text}`, [])
+});
+
+bot.onText('',async(msg)=>{
+  await handleMovieRequest(msg,`${msg.text}`, [])
+});
+
+bot.onText('',async(msg)=>{
+  await handleMovieRequest(msg,`${msg.text}`, [])
+});
